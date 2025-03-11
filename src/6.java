973bc0031b1faf1185ed97fc4ccabc3cf2f1f127
michaelisstrehl768 <michaelisstrehl768@gmail.com>
2025-03-11 04:55:34 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,7 @@
+// Random Java Code
+public class Main {
+    public static void main(String[] args) {
+        int x = (int)(Math.random() * 10);
+        System.out.println("Random number between 0 and 9: " + x);
+    }
+}
