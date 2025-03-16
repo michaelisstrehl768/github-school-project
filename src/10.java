e3cdf38347b7cfae687f199d06d93519d81bcf42
michaelisstrehl768 <michaelisstrehl768@gmail.com>
2025-03-16 21:09:00 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Code{
+ public static void main(String[] args){
+  String[] names = {"Rakesh", "Mayank", "Karan"};
+  for (int i=0; i<names.length; i++){
+   System.out.println(names[i]);
+  }
+ }
+}
