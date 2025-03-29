69cd4156cc420463a89acb10f83d24341bb3fcdb
michaelisstrehl768 <michaelisstrehl768@gmail.com>
2025-03-29 06:08:00 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,9 @@
+public class SchoolProjectCode {
+    public static void main(String[] args) {
+        // This line of code generates a random number between 1 and 100.
+        int randomNumber = (int)(Math.random() * 100);
+        
+        // This block of code prints the random number to the console.
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
