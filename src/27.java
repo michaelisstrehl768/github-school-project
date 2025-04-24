dcaa9e11f8cad6f269127ddb72a5e0b4ece75320
michaelisstrehl768 <michaelisstrehl768@gmail.com>
2025-04-24 15:21:47 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,7 @@
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Example of generating a random integer between 1 and 10 (inclusive)
+        int randomNumber = (int)(Math.random() * 10 + 1);
+        System.out.println("Random number: " + randomNumber);
+    }
+}
