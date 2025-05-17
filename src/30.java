720c6333a929352c9bf0ad524223b8216ebf74f5
michaelisstrehl768 <michaelisstrehl768@gmail.com>
2025-05-17 05:58:04 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,6 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Code to be executed in this method
+        System.out.println("Hello from School Project!");
+    }
+}
